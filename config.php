3370ed5fe540cad2dc51743c60b92909700f4f0a
LukaKoporec <Luka.koporec123@gmail.com>
2021-03-12 15:51:01 +0100

    Created Models

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+//define DB Params
+
+define("DB_HOST", "localhost");
+define("DB_USER", "Luka");
+define("DB_PASS", "12345");
+define("DB_NAME", "shareboard");
+
+// define URL
+define("ROOT_PATH", "/");
+define("ROOT_URL", "http://localhost/php.dev");
+ ?>
