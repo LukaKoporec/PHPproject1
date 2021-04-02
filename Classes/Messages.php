b245434d6b49b57048471fe8143e0e44d517b512
LukaKoporec <58914727+LukaKoporec@users.noreply.github.com>
2021-04-02 12:15:08 +0200

    Update Classes/Messages.php
    Co-authored-by: Slaven Stančič <stancic.slaven@gmail.com>

--- Classes/Messages.php
+++ Classes/Messages.php
@@ -11,7 +11,7 @@ class Messages{
             echo '<div class="alert alert-danger">'.$_SESSION['errorMsg'].'</div>';
             unset($_SESSION['errorMsg']);
         }
-        if(isset($_SESSION['successMsg'])){
+        if(isset($_SESSION['successMsg'])) {
             echo '<div class="alert alert-success">'.$_SESSION['successMsg'].'</div>';
             unset($_SESSION['successMsg']);
         }
